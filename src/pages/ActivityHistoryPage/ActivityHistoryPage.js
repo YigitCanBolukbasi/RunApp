c27fcbc3ef391b1308da6bd323a65d1df88ee1e5
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-02 16:14:23 +0300

    Activity History Implemented

---
+++ src/pages/ActivityHistoryPage/ActivityHistoryPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './ActivityHistoryPage.styles';
+
+function ActivityHistoryPage() {
+  return (
+    <View>
+      <Text>Activity History</Text>
+    </View>
+  );
+}
+
+export default ActivityHistoryPage;
