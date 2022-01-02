6c2da837f34eb1e4843c01f61648bb92732b9708
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-02 16:14:51 +0300

    Leader Board Implemented

---
+++ src/pages/LeaderBoard/LeaderBoard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import styles from './LeaderBoard.styles';
+
+function LeaderBoard() {
+  return (
+    <View>
+      <Text> LeaderBoard </Text>
+    </View>
+  );
+}
+
+export default LeaderBoard;
