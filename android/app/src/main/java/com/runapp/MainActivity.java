16896d98e06841d7ada871e8072f12fe493d1460
Yigit Can <y.can.bolukbasi@gmail.com>
2022-01-09 12:52:46 +0300

    Splash Screen Fixed

--- android/app/src/main/java/com/runapp/MainActivity.java
+++ android/app/src/main/java/com/runapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.runapp;
 
 import com.facebook.react.ReactActivity;
 import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,6 +12,7 @@ public class MainActivity extends ReactActivity {
    */
 @Override
 protected void onCreate(Bundle savedInstanceState) {
+  SplashScreen.show(this); 
   super.onCreate(null);
 }
 
--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import * as React from 'react';
+import React, {useEffect} from 'react';
 import Navigation from './navigation/Navigation';
 import {AuthProvider} from './navigation/AuthProvider';
+import SplashScreen from 'react-native-splash-screen';
 
 function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
   return (
     <AuthProvider>
       <Navigation />
